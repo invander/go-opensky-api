66324f4d168fbbea8e30d96ee19e08eecef839ba
Alex <invander@gmail.com>
2023-03-01 14:08:19 +0300

    remove spaces from callsign

--- model.go
+++ model.go
@@ -1,6 +1,10 @@
 package opensky
 
-import "time"
+import (
+	"bytes"
+	"strings"
+	"time"
+)
 
 type errorResponse struct {
 	Timestamp int64  `json:"timestamp"`
@@ -11,19 +15,28 @@ type errorResponse struct {
 	Path      string `json:"path"`
 }
 
+type CallSignTrim string
+
 type Flight struct {
-	Icao24                           string `json:"icao24"`
-	FirstSeen                        int    `json:"firstSeen"`
-	EstDepartureAirport              string `json:"estDepartureAirport"`
-	LastSeen                         int    `json:"lastSeen"`
-	EstArrivalAirport                string `json:"estArrivalAirport"`
-	Callsign                         string `json:"callsign"`
-	EstDepartureAirportHorizDistance int    `json:"estDepartureAirportHorizDistance"`
-	EstDepartureAirportVertDistance  int    `json:"estDepartureAirportVertDistance"`
-	EstArrivalAirportHorizDistance   int    `json:"estArrivalAirportHorizDistance"`
-	EstArrivalAirportVertDistance    int    `json:"estArrivalAirportVertDistance"`
-	DepartureAirportCandidatesCount  int    `json:"departureAirportCandidatesCount"`
-	ArrivalAirportCandidatesCount    int    `json:"arrivalAirportCandidatesCount"`
+	Icao24                           string       `json:"icao24"`
+	FirstSeen                        int          `json:"firstSeen"`
+	EstDepartureAirport              string       `json:"estDepartureAirport"`
+	LastSeen                         int          `json:"lastSeen"`
+	EstArrivalAirport                string       `json:"estArrivalAirport"`
+	Callsign                         CallSignTrim `json:"callsign"`
+	EstDepartureAirportHorizDistance int          `json:"estDepartureAirportHorizDistance"`
+	EstDepartureAirportVertDistance  int          `json:"estDepartureAirportVertDistance"`
+	EstArrivalAirportHorizDistance   int          `json:"estArrivalAirportHorizDistance"`
+	EstArrivalAirportVertDistance    int          `json:"estArrivalAirportVertDistance"`
+	DepartureAirportCandidatesCount  int          `json:"departureAirportCandidatesCount"`
+	ArrivalAirportCandidatesCount    int          `json:"arrivalAirportCandidatesCount"`
+}
+
+func (c *CallSignTrim) UnmarshalJSON(data []byte) error {
+	data = bytes.Trim(data, "\"")
+	data = bytes.Trim(data, " ")
+	*c = CallSignTrim(strings.TrimSpace(string(data)))
+	return nil
 }
 
 // Unstructured raw response for tracks queries.
